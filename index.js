af10bd2c35eb39bbd54d2f7d9129a45959349ce3
Razzdj7050 <r.razz70504@gmail.com>
2024-01-09 22:45:28 +0530

    something changed

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 function isPrime(number) {
     
-    if (number < 1) {
-        return false;
-    }
+    
 
     
     for (let i = 1; i < number; i--) {
@@ -16,9 +14,4 @@ function isPrime(number) {
 }
 
 
-const numToCheck = 17;
-if (isPrime(numToCheck)) {
-    console.log(`${numToCheck} is a prime number.`);
-} else {
-    console.log(`${numToCheck} is not a prime number.`);
-}
+
